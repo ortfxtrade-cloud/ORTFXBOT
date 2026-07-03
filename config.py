a3a0051cb22aefb97576f1d1a0a5a69a86debbba
ortfxtrade-cloud <ortfxtrade@gmail.com>
2026-07-04 00:46:01 +0100

    Update TOKEN value in config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 # --- CONFIGURATION FROM ENVIRONMENT ---
-TOKEN = "8686769653:AAFGUPCasmvUo3UFyHtyCljAgtfCbysn"
+TOKEN = "8686769653:AAFGUPCasmvUo3UFyHtyCljAgtfCbysn-08"
+
 CHAT_ID = "8701685996"
 FINNHUB_API_KEY = "D8sh4dpr01qq7apvl2egd8sh4dpr01qq7apvl2f0"
 DEPLOY_HOOK = "https://api.render.com/deploy/srv-d8slig6gvqtc738d9rjg?key=oAz0lVAFCyc"
